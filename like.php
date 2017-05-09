bd836ac71c5315d923425dd276e72eedfa076872
al <al@notearise.com>
2017-05-09 19:32:27 -0400

    71
    Unlike handler

--- like.php
+++ like.php
@@ -33,11 +33,12 @@
 
   	$user_details_array = mysqli_query($con,"SELECT * FROM users WHERE username = '$user_liked'");
   	$row = mysqli_fetch_array($user_details_query);
+		$total_user_likes = $row['num_likes'];
 
 	  // Like button
 		if(isset($_POST['like_button'])) {
 			$total_likes++;
-			$query = mysqli_query($con, "UPDATE posts SET likes = '$total_likes' WHERE post_id = '$post_id'");
+			$query = mysqli_query($con, "UPDATE posts SET likes = '$total_likes' WHERE id = '$post_id'");
 			$total_user_likes++;
 			$user_likes = mysqli_query($con, "UPDATE users SET num_likes = '$total_user_likes' WHERE username = '$user_liked'");
 			$insert_user = mysqli_query($con, "INSERT INTO likes VALUES (NULL,'$userLoggedIn','$post_id')");
@@ -45,7 +46,16 @@
 			// Insert notification
 		}
 
-	  // Unlike button
+		// Unlike button
+		if(isset($_POST['unlike_button'])) {
+			$total_likes--;
+			$query = mysqli_query($con, "UPDATE posts SET likes = '$total_likes' WHERE id = '$post_id'");
+			$total_user_likes--;
+			$user_likes = mysqli_query($con, "UPDATE users SET num_likes = '$total_user_likes' WHERE username = '$user_liked'");
+			$insert_user = mysqli_query($con, "DELETE FROM likes WHERE username = '$userLoggedIn' AND post_id = '$post_id'");
+
+			// Insert notification
+		}
 
 	  // Check for previous likes
 	  $check_query = mysqli_query($con,"SELECT * FROM likes WHERE username = '$userLoggedIn' AND post_id = '$post_id'");
@@ -54,7 +64,7 @@
 		// ...
 	  if($num_rows > 0) {
 	    echo '<form action="like.php?post_id=' . $post_id . '" method="POST">
-							<input type="submit" class="comment_like" name="unlike_button" value="Unike">
+							<input type="submit" class="comment_like" name="unlike_button" value="Unlike">
 							<div class="like_value">
 								'. $total_likes .' Likes
 							</div>
--- nz_friday_likes.sql
+++ nz_friday_likes.sql
@@ -5,5 +5,9 @@ CREATE TABLE likes (
 	username VARCHAR(60) COMMENT 'username of user posting',
 	post_id INT COMMENT 'id of post'
 );
-
+	
 SELECT * FROM likes;
+
+SELECT likes,added_by FROM likes WHERE id='100110';
+INSERT INTO likes VALUES (100110')
+
