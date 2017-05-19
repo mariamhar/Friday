137550d979896b22cc5c130d4e8353c0850dd7d1
al <al@notearise.com>
2017-05-19 16:27:20 -0400

    92 - Upload profile pic
    92 - in order to upload profile pic I had to remove
    $_SERVER[‘Document_Root’] because it was listing a server name that
    didn’t exist. “Library/WebContent/Documents/…” This is not the server
    name. My guess is that this is the default server name and I need to go
    into the PHP.ini file to update it.

--- includes/header.php
+++ includes/header.php
@@ -23,8 +23,11 @@ else {
 	<script src="assets/js/bootstrap.js"></script>
 	<script src="assets/js/bootbox.min.js"></script>
 	<script src="assets/js/friday.js"></script>
+	<script src="assets/js/jquery.Jcrop.js"></script>
+	<script src="assets/js/jcrop_bits.js"></script>
 
 	<!-- CSS -->
+	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
