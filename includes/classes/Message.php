a399a9b546ec20a3702f8d1e91cdddcfe459eaa8
al <al@notearise.com>
2017-05-22 14:07:48 -0400

    100
    100 - loaded messages on to the page… shit is boss

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -40,15 +40,29 @@
       }
     }
 
-    public function getMessages() {
+    public function getMessages($otherUser) {
 
       $userLoggedIn = $this->user_obj->getUsername();
 
-      $date = "";
+      $data = "";
 
-      $query = mysqli_query($this->con, "UPDATE messages SET opened = 'yes' WHERE user_to = '$userLoggedIn' AND user_from = '$other_user'");
+      $query = mysqli_query($this->con, "UPDATE messages SET opened = 'yes' WHERE user_to = '$userLoggedIn' AND user_from = '$otherUser'");
 
       $get_messages_query = mysqli_query($this->con, "SELECT * FROM messages WHERE (user_to = '$userLoggedIn' AND user_from = '$otherUser') OR (user_from = '$userLoggedIn' AND user_to = '$otherUser')");
+
+      while($row = mysqli_fetch_array($get_messages_query)) {
+
+        $user_to = $row['user_to'];
+        $user_from = $row['user_from'];
+        $body = $row['body'];
+
+        $div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green'>" : "<div class='message' id='blue'>";
+        $data = $data . $div_top . $body . "</div><br>";
+
+      }
+
+      return $data;
+
     }
 
   }
