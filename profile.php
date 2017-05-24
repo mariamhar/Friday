b3670bc18b8276725a6ea49293e2c75303d3f681
al <al@notearise.com>
2017-05-24 13:15:35 -0400

    113
    113 completed loading the messages on the profile page but I still
    can’t send anything. My profile.php isn’t working but the other one is…
    Couple issues… Message area is loading towards the bottom of the page
    near the last “Newsfeed” load 2) on message submission (attempt) the
    newsfeed tab is being set to active and the submission isn’t going
    through. Need to fix.

--- profile.php
+++ profile.php
@@ -90,19 +90,44 @@
 
       <ul class="nav nav-tabs" role="tablist" id="profileTabs">
         <li role="presentation" class="active"><a href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab">Newsfeed</a></li>
-        <li role="presentation"><a href="#about_div" aria-controls="about_div" role="tab" data-toggle="tab">About</a></li>
+        <!--// <li role="presentation"><a href="#about_div" aria-controls="about_div" role="tab" data-toggle="tab">About</a></li>//-->
         <li role="presentation"><a href="#messages_div" aria-controls="messages_div" role="tab" data-toggle="tab">Messages</a></li>
       </ul>
 
-      <div class="tab_content">
+      <div class="tab-content">
         <div role="tabpanel" class="tab-pane fade in active" id="newsfeed_div">
             <div class="posts_area"></div>
         </div>
+
+        <!--//
         <div role="tabpanel" class="tab-pane fade" id="about_div">
 
         </div>
+        //-->
         <div role="tabpanel" class="tab-pane fade" id="messages_div">
-            
+          <?php
+
+            $message_obj = new Message($con, $userLoggedIn);
+
+            echo "<h4>You and <a href='" . $username . "'>" . $profile_user_obj->getFirstAndLastName() . "</a></h4><hr><br>";
+            echo "<div class='loaded_messages' id='scroll_messages'>";
+              echo $message_obj->getMessages($username);
+            echo "</div>";
+
+          ?>
+
+          <div class="message_post">
+            <form action="" method="POST">
+              <textarea name='message_body' id='message_textarea' placeholder='Write your message...'></textarea>
+              <input type='submit' name='post_message' class='info' id='message_submit' value='Send'>
+            </form>
+
+          </div>
+
+          <script>
+            var div = document.getElementById("scroll_messages");
+            div.scrollTop = div.scrollHeight;
+          </script>
         </div>
       </div>
 
@@ -166,7 +191,7 @@
     					type: "POST",
     					data: "page=" + page + "&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
     					cache:false,
-    					success: function(response) {
+    					success: function(response)  {
     						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage
     						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage
     						$('#loading').hide();
