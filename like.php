7b10263622f0ec4e1219d8ff8157d0d1d7e33868
al <al@notearise.com>
2017-06-02 02:41:51 -0500

    132 - the real 132 this time
    Accidentally named the video before this incorrectly.

--- like.php
+++ like.php
@@ -24,6 +24,7 @@
 		require 'config/config.php';
 		include("includes/classes/User.php");
 		include("includes/classes/Post.php");
+		include("includes/classes/Notification.php");
 
 		// ...
 		if (isset($_SESSION['username'])) {
@@ -60,7 +61,7 @@
 
 			// Insert notification
 			if($user_liked != $userLoggedIn) {
-				$notification = new Notification($this->con, $userLoggedIn);
+				$notification = new Notification($con, $userLoggedIn);
 				$notification->insertNotification($post_id, $user_to, "like");
 			}
 		}
