73eabb75c011c60c9583a447ecf4ca180da389b7
al <al@notearise.com>
2017-06-07 19:43:25 -0400

    156
    156 - SHEESH

--- search.php
+++ search.php
@@ -55,22 +55,96 @@
 
 				$names = explode(" ", $query);
 
-				// If there are 2 words assume they are first and last names respectively
 				if(count($names) == 3) {
-					$usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[2]%') AND user_closed = 'no'");
+					$usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[2]%') AND user_closed='no'");
+				//If query has one word only, search first names or last names
 				}
 
 				else if(count($names) == 2) {
-					$usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') AND user_closed = 'no'");
+					$usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') AND user_closed='no'");
 				}
 
 				else {
-					$usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[1]%') AND user_closed = 'no'");
+					$usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[0]%') AND user_closed='no'");
+				}
+			}
+
+			// Check if results were found
+			if(mysqli_num_rows($usersReturnedQuery) == 0) {
+				echo "We can't find anyone with a " . $type . " like: " . $query;
+			}
+
+			else {
+				echo mysqli_num_rows($usersReturnedQuery) . " results found: <br/><br/>";
+			}
+
+			echo "<p id='grey'>Try searching for:</p>";
+			echo "<a href='search.php?q=" . $query . "&type=name'>Names</a>, <a href='search.php?q=" . $query . "&type=username'>Usernames</a>";
+
+			while($row = mysqli_fetch_array($usersReturnedQuery)) {
+				$user_obj = new User($con, $user['username']);
+
+				$button = "";
+				$mutual_friends = "";
+
+				if($user['username'] != $row['username']) {
+
+					// Generate button depending on friendship status
+					if($user_obj->isFriend($row['username'])) {
+						$button = "<input type='submit' name='" . $row['username'] . "' class='danger' value='Remove Friend'>";
+					}
+					else if($user_obj->didReceiveRequest($row['username'])) {
+						$button = "<input type='submit' name='" . $row['username'] . "' class='warning' value='Respond to request'>";
+					}
+					else if($user_obj->didSendRequest($row['username'])) {
+						$button = "<input class='default' value='Request Sent'>";
+					}
+					else {
+						$button = "<input type='submit' name='" . $row['username'] . "' class='success' value='Add Friend'>";
+					}
+
+					$mutual_friends = $user_obj->getMutualFriends($row['username']) . " friends in common";
+
+					// Button forms
+
 				}
 
-			}
+				echo 	"
+								<div class='search_result'>
 
-			}
+									<div>
+
+										<form action='' method='POST'>
+											" . $button . "
+											<br/>
+										</form>
+
+									</div>
+
+									<div>
+
+										<a href='" . $row['username'] . "'>
+											<img src='" . $row['profile_pic'] . "' style='height:100px;'>
+										</a>
+
+										<a href='" . $row['username'] . "'>
+											" . $row['first_name'] . " " . $row['last_name'] . "
+											<p id='grey'>" . $row['username'] .  "</p>
+										</a>
+
+										<br />
+
+										" . $mutual_friends . "
+
+										<br />
+
+									</div>
+
+								</div>
+								<hr />
+							";
+
+			} // End while
 
 		}
 
