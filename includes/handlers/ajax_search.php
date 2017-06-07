f0f3ccfb6ad6a531ef2bd45583e6daf643c67be3
al <al@notearise.com>
2017-06-07 15:57:06 -0400

    146
    146 - getting the AJAX set up for quick search

---
+++ includes/handlers/ajax_search.php
@@ -0,0 +1,13 @@
+<?php
+
+	include("../../config/config.php");
+	include("../../includes/classes/User.php");
+
+	$query = $_POST['query'];
+	$userLoggedIn = $_POST['userLoggedIn'];
+
+	$names = explode(" ", $query);
+
+		
+
+?>
