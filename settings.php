6fb59d4d332221e1fbf29360cf04d723e53fb41b
al <al@notearise.com>
2017-06-07 22:42:21 -0400

    159

--- settings.php
+++ settings.php
@@ -20,5 +20,24 @@
 
   <a href='upload.php'>Upload a new profile picture</a><br/><br/><br/>
 
+  Modify the values and click 'Update Details'
+
+  <form action='settings.php' method='POST'>
+    First Name: <input type='text' name='first_name' value='<?php echo $user['first_name']; ?>'><br/>
+    Last Name: <input type='text' name='last_name' value='<?php echo $user['last_name']; ?>'><br/>
+    Email: <input type='text' name='email' value='<?php echo $user['email']; ?>'><br/>
+  </form>
+
+  <h4>Change Password</h4>
+  <form action='settings.php' method='POST'>
+    Old Password: <input type='password' name='old_password'><br/>
+    New Password: <input type='password' name='new_password_1'><br/>
+    New Password Again: <input type='password' name='new_password_2'><br/>
+  </form>
+
+  <h4>Close Account</h4>
+  <form action="settings.php">
+    <input type="submit" name="close_account" id="close_account" value="Close Account">
+  </form>
 
 </div>
