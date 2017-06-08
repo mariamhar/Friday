3a7338cfe398565ebd348ddb4c007ac87ea4532e
al <al@notearise.com>
2017-06-08 00:16:06 -0400

    160
    160 - Adding Functionality to the Update Details Form

--- settings.php
+++ settings.php
@@ -26,14 +26,20 @@
     First Name: <input type='text' name='first_name' value='<?php echo $user['first_name']; ?>'><br/>
     Last Name: <input type='text' name='last_name' value='<?php echo $user['last_name']; ?>'><br/>
     Email: <input type='text' name='email' value='<?php echo $user['email']; ?>'><br/>
+    <input type="submit" name="update_details" id="save_details" value="Update Details">
   </form>
+
+  <br/>
 
   <h4>Change Password</h4>
   <form action='settings.php' method='POST'>
     Old Password: <input type='password' name='old_password'><br/>
     New Password: <input type='password' name='new_password_1'><br/>
     New Password Again: <input type='password' name='new_password_2'><br/>
+    <input type="submit" name="update_password" id="update_password" value="Update Password">
   </form>
+
+  <br/>
 
   <h4>Close Account</h4>
   <form action="settings.php">
