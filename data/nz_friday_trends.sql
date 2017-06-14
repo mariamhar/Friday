3da4c94658e632d3a8eb9e731618a90e6729ee09
al <al@notearise.com>
2017-06-14 16:24:47 -0400

    Quickie

--- data/nz_friday_trends.sql
+++ data/nz_friday_trends.sql
@@ -11,10 +11,31 @@ ALTER TABLE `trends`
 ALTER TABLE `trends`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
-SET @term = 'cosby';
+SET @term = 'cnn';
 SELECT @term;
 SELECT * FROM trends;
-SELECT * FROM trends WHERE title = @term;
+SELECT * FROM trends WHERE title = '@term';
 INSERT INTO trends (id,title,hits) VALUES (NULL, @term, '1');
 UPDATE trends SET hits = hits + 1 WHERE title = '@term';
 UPDATE trends SET hits = hits + 1 WHERE title = 'covfefe';
+SELECT * FROM trends ORDER BY hits DESC LIMIT 10;
+
+INSERT INTO `trends` (`id`, `title`, `hits`) VALUES
+(28, 'covfefe', 1),
+(29, 'cosby', 1),
+(30, 'heard', 1),
+(31, 'trinidad', 1),
+(32, 'Frankly', 2),
+(33, 'Insulting', 1),
+(34, 'Considering', 1),
+(35, 'President', 4),
+(36, 'Liar', 2),
+(37, 'Shame', 1),
+(38, 'Americans', 2),
+(39, 'American', 1),
+(40, 'People', 1),
+(41, 'Themselves', 1),
+(42, 'Fellow', 1),
+(43, 'Sound', 1),
+(44, 'Acumen', 1),
+(45, 'Past', 1);
